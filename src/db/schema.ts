3890847b725ab5560b982baaac9aedd29d96bf43
charaekeow <charaekeow11@gmail.com>
2024-11-03 10:43:05 +0800

    feat: setup drizzle, next, & ts; add user & session schema & migration

---
+++ src/db/schema.ts
@@ -0,0 +1,20 @@
+import { serial, integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { InferSelectModel } from 'drizzle-orm';
+
+export const userTable = pgTable('user', {
+	id: serial('id').primaryKey(),
+});
+
+export const sessionTable = pgTable('session', {
+	id: text('id').primaryKey(),
+	userId: integer('user_id')
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: timestamp('expires_at', {
+		withTimezone: true,
+		mode: 'date',
+	}).notNull(),
+});
+
+export type User = InferSelectModel<typeof userTable>;
+export type Session = InferSelectModel<typeof sessionTable>;
